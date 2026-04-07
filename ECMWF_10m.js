f25ddadbc1a4cbbdf3fed5aaac36792cc8611b51
eknlau5897 <136979484+eknlau5897@users.noreply.github.com>
2026-04-07 14:06:49 +0800

    update

--- ECMWF_10m.js
+++ ECMWF_10m.js
@@ -7,7 +7,6 @@ canvas.height = canvas.clientHeight;
 const gl = canvas.getContext('webgl', {antialiasing: false});
 
 const wind = window.wind = new WindGL(gl);
-wind.numParticles = 65536;
 
 function frame() {
     if (wind.windData) {
@@ -16,10 +15,20 @@ function frame() {
     requestAnimationFrame(frame);
 }
 frame();
+const center = [114.17, 22.35]; 
+const zoomLevel = 400.0; // Higher = more zoomed in
+
+// 2. Pass them to the shader as uniforms
+const program = wind.drawProgram;
+gl.useProgram(program.program);
+
+// u_offset is the negative of the center to "pull" HK to (0,0)
+gl.uniform2f(program.u_offset, -center[0], -center[1]);
+gl.uniform1f(program.u_zoom, zoomLevel);
 
 const gui = new dat.GUI();
 gui.add(wind, 'numParticles', 1024, 589824);
-gui.add(wind, 'fadeOpacity', 0.96, 0.999,0.001).updateDisplay();
+gui.add(wind, 'fadeOpacity', 0.96, 0.999, 0.001).updateDisplay();
 gui.add(wind, 'speedFactor', 0.05, 1.0);
 gui.add(wind, 'dropRate', 0, 0.1);
 gui.add(wind, 'dropRateBump', 0, 0.2);
@@ -33,19 +42,38 @@ const windFiles = {
 };
 
 const meta = {
-    '2026-4-5+12Z': 0,
+    'zoom': 2,
+    '2026-04-06+h': 0,
     'retina resolution': true,
-    'change to GFS_100m wind': function () {
-        window.location = 'https://eknlau5897.github.io/Earth_windy/index.html';
+    'change to GFS 100m wind': function () {
+        window.location = 'http://eknlau5897.github.io/Earth_windy/index.html';
     }
 };
-gui.add(meta, '2026-4-5+12Z', 0, 24, 6).onFinishChange(updateWind);
+
+gui.add(meta, 'zoom', 2, 10, 0.01).onChange(updateZoom);
+gui.add(meta, '2026-04-05+12Z', 0, 48, 24).onFinishChange(updateWind);
+
 if (pxRatio !== 1) {
     gui.add(meta, 'retina resolution').onFinishChange(updateRetina);
 }
-gui.add(meta, 'change to GFS_100m wind');
+
+gui.add(meta, 'http://eknlau5897.github.io/Earth_windy/index.html');
+
 updateWind(0);
 updateRetina();
+
+function updateZoom() {
+    const halfSize = 0.5 / Math.pow(2, meta.zoom);
+    wind.setView([
+        0.8171 - halfSize,
+        0.62 - halfSize,
+        0.8171 + halfSize,
+        0.62 + halfSize
+    ]);
+    drawCoastline();
+    wind.resize();
+    wind.numParticles = wind.numParticles;
+}
 
 function updateRetina() {
     const ratio = meta['retina resolution'] ? pxRatio : 1;
@@ -54,7 +82,14 @@ function updateRetina() {
     wind.resize();
 }
 
+let coastlineFeatures;
+
 getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_coastline.geojson', function (data) {
+    coastlineFeatures = data.features;
+    drawCoastline();
+});
+
+function drawCoastline() {
     const canvas = document.getElementById('coastline');
     canvas.width = canvas.clientWidth * pxRatio;
     canvas.height = canvas.clientHeight * pxRatio;
@@ -65,24 +100,32 @@ getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_coastl
     ctx.strokeStyle = 'white';
     ctx.beginPath();
 
-    for (let i = 0; i < data.features.length; i++) {
-        const line = data.features[i].geometry.coordinates;
+    for (let i = 0; i < coastlineFeatures.length; i++) {
+        const line = coastlineFeatures[i].geometry.coordinates;
         for (let j = 0; j < line.length; j++) {
+            const x = (line[j][0] + 180) / 360;
+            const y = latY(line[j][1]);
+            const minX = wind.bbox[0];
+            const minY = latY(180 * wind.bbox[3] - 90);
+            const maxX = wind.bbox[2];
+            const maxY = latY(180 * wind.bbox[1] - 90);
             ctx[j ? 'lineTo' : 'moveTo'](
-                (line[j][0] + 180) * canvas.width / 360,
-                (-line[j][1] + 90) * canvas.height / 180);
+                (x - minX) / (maxX - minX) * canvas.width,
+                (y - minY) / (maxY - minY) * canvas.height);
         }
     }
     ctx.stroke();
-});
+}
 
 function updateWind(name) {
-    getJSON('./ec_10m_wind/' + windFiles[name] + '.json', function (windData) {
+    getJSON('wind/' + windFiles[name] + '.json', function (windData) {
         const windImage = new Image();
-        windData.image = windImage;
-        windImage.src = './ec_10m_wind/' + windFiles[name] + '.png';
+        windImage.src = 'wind/' + windFiles[name] + '.png';
         windImage.onload = function () {
-            wind.setWind(windData);
+            // Must define the geographic bounds of the wind image
+            wind.bbox = windData.bbox || [113.8, 22.1, 114.5, 22.6]; 
+            wind.setWind(windData, windImage);
+            updateZoom();
         };
     });
 }
@@ -99,4 +142,11 @@ function getJSON(url, callback) {
         }
     };
     xhr.send();
+}
+
+function latY(lat) {
+    const sin = Math.sin(lat * Math.PI / 180),
+        y = (0.5 - 0.25 * Math.log((1 + sin) / (1 - sin)) / Math.PI);
+    return y < 0 ? 0 :
+           y > 1 ? 1 : y;
 }
