ea8c16878495219ee1e157239952534ceae0d12c
eknlau5897 <136979484+eknlau5897@users.noreply.github.com>
2026-04-08 10:34:03 +0800

    Add files via upload

---
+++ GFS_100m/index.js
@@ -0,0 +1,119 @@
+// using var to work around a WebKit bug
+var canvas = document.getElementById('canvas'); // eslint-disable-line
+
+const pxRatio = Math.max(Math.floor(window.devicePixelRatio) || 1, 2);
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
+const gl = canvas.getContext('webgl', {antialiasing: false});
+
+const wind = window.wind = new WindGL(gl);
+wind.numParticles = 65536;
+
+function frame() {
+    if (wind.windData) {
+        wind.draw();
+    }
+    requestAnimationFrame(frame);
+}
+frame();
+
+const gui = new dat.GUI();
+gui.add(wind, 'numParticles', 1024, 589824);
+gui.add(wind, 'fadeOpacity', 0.96, 0.999).step(0.001).updateDisplay();
+gui.add(wind, 'speedFactor', 0.05, 1.0);
+gui.add(wind, 'dropRate', 0, 0.1);
+gui.add(wind, 'dropRateBump', 0, 0.2);
+
+const windFiles = {
+    0: '2026040718_00',
+    6: '2026040718_06',
+    12: '2026040718_12',
+    18: '2026040718_18',
+    24: '2026040718_24',
+    30: '2026040718_30',
+    36: '2026040718_36',
+    42: '2026040718_42',
+    48: '2026040718_48',
+    54: '2026040718_54',
+    60: '2026040718_60',
+    66: '2026040718_66',
+    72: '2026040718_72',
+    78: '2026040718_78',
+    84: '2026040718_84',
+    90: '2026040718_90',
+    96: '2026040718_96',
+    102: '2026040718_102',
+    108: '2026040718_108',
+    114: '2026040718_114',
+    120: '2026040718_120'
+};
+
+const meta = {
+    '2026-04-07+18Z': 0,
+    'retina resolution': true,
+    'github.com/mapbox/webgl-wind': function () {
+        window.location = 'https://github.com/mapbox/webgl-wind';
+    }
+};
+gui.add(meta, '2026-04-07+18Z', 0, 120, 6).onFinishChange(updateWind);
+if (pxRatio !== 1) {
+    gui.add(meta, 'retina resolution').onFinishChange(updateRetina);
+}
+gui.add(meta, 'github.com/mapbox/webgl-wind');
+updateWind(0);
+updateRetina();
+
+function updateRetina() {
+    const ratio = meta['retina resolution'] ? pxRatio : 1;
+    canvas.width = canvas.clientWidth * ratio;
+    canvas.height = canvas.clientHeight * ratio;
+    wind.resize();
+}
+
+getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_coastline.geojson', function (data) {
+    const canvas = document.getElementById('coastline');
+    canvas.width = canvas.clientWidth * pxRatio;
+    canvas.height = canvas.clientHeight * pxRatio;
+
+    const ctx = canvas.getContext('2d');
+    ctx.lineWidth = pxRatio;
+    ctx.lineJoin = ctx.lineCap = 'round';
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+
+    for (let i = 0; i < data.features.length; i++) {
+        const line = data.features[i].geometry.coordinates;
+        for (let j = 0; j < line.length; j++) {
+            ctx[j ? 'lineTo' : 'moveTo'](
+                (line[j][0] + 180) * canvas.width / 360,
+                (-line[j][1] + 90) * canvas.height / 180);
+        }
+    }
+    ctx.stroke();
+});
+
+function updateWind(name) {
+    getJSON('wind/' + windFiles[name] + '.json', function (windData) {
+        const windImage = new Image();
+        windData.image = windImage;
+        windImage.src = 'wind/' + windFiles[name] + '.png';
+        windImage.onload = function () {
+            wind.setWind(windData);
+        };
+    });
+}
+
+function getJSON(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('get', url, true);
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            callback(xhr.response);
+        } else {
+            throw new Error(xhr.statusText);
+        }
+    };
+    xhr.send();
+}
